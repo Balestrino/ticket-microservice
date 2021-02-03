cf96f39d074eb38a77d266c24db9904be704df31
Balestrino <salvatore.balestrino@gmail.com>
2021-02-03 17:18:54 +0000

    client update

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -4,7 +4,7 @@ import { natsWrapper } from './nats-wrapper';
 import { TicketCreatedListener } from './events/listeners/ticket-created-listener';
 import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
 import { ExpirationCompleteListener } from './events/listeners/expiration-complete-listener';
-import { PaymentCreatedListener } from './events/listeners/payment-created-listners';
+import { PaymentCreatedListener } from './events/listeners/payment-created-listener';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,10 +3,10 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@gadue/common';
-import { createTicketRouter } from './routes/new'
+import { createTicketRouter } from './routes/new';
 import { showTicketRouter } from './routes/show';
-import { indexTicketRouter } from './routes/index'
-import { updateTicketRouter } from './routes/update'
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
 app.set('trust proxy', true);
@@ -14,11 +14,9 @@ app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: false
-    // secure: process.env.NODE_ENV !== "test"
+    secure: process.env.NODE_ENV !== 'test',
   })
 );
-
 app.use(currentUser);
 
 app.use(createTicketRouter);
